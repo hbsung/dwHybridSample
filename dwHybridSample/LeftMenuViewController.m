b8e804f8c15991de5c6449740e2b780b5bc18695
성효빈 <zircon2@naver.com>
2015-11-04 18:06:17 +0900

    delete dummy lines

--- dwHybridSample/LeftMenuViewController.m
+++ dwHybridSample/LeftMenuViewController.m
@@ -13,37 +13,12 @@
 
 #pragma mark - IBAction
 
-//- (IBAction)onClickSettingButton:(id)sender {
-//    UIStoryboard *mainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-//    UITableViewController *settingTVC = [mainStoryBoard instantiateViewControllerWithIdentifier:@"SettingTableViewController"];
-//    SlideNavigationController *slideSharedInstance = [SlideNavigationController sharedInstance];
-//    
-//    [UIView  beginAnimations: @"Showinfo"context: nil];
-//    [UIView setAnimationCurve: UIViewAnimationCurveEaseOut];
-//    [UIView setAnimationDuration:0.75];
-//    [slideSharedInstance pushViewController: settingTVC animated:NO];
-//    [UIView setAnimationTransition:UIViewAnimationTransitionCurlUp forView:slideSharedInstance.view cache:NO];
-//    [UIView commitAnimations];
-//    
-//    
-//    [[SlideNavigationController sharedInstance] pushViewController:settingTVC animated:NO];
-//}
-
-
 - (IBAction)onClickSettingButton:(id)sender {
     UIStoryboard *mainStoryBoard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     UITableViewController *settingTVC = [mainStoryBoard instantiateViewControllerWithIdentifier:@"SettingTableViewController"];
-    
     SlideNavigationController *slideSharedInstance = [SlideNavigationController sharedInstance];
-//    [slideSharedInstance closeMenuWithCompletion:^{
-//        [slideSharedInstance setLeftMenu: settingTVC];
-//        [slideSharedInstance openMenu:MenuLeft withCompletion:nil];
-//    }];
     
     [slideSharedInstance pushViewController:settingTVC animated:YES];
 
-//    [[SlideNavigationController sharedInstance] pushViewController:settingTVC animated:YES];
-//    [[SlideNavigationController sharedInstance] popToRootAndSwitchToViewController:settingTVC withCompletion:nil];
-//    [[SlideNavigationController sharedInstance] popAllAndSwitchToViewController:settingTVC withCompletion:nil];
 }
 @end
