843a6f7539798e5569925b94f5a0c5d23abd0027
성효빈 <zircon2@naver.com>
2015-11-04 13:49:55 +0900

    Initial Commit

---
+++ dwHybridSample/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  dwHybridSample
+//
+//  Created by 성효빈 on 2015. 11. 4..
+//  Copyright © 2015년 dwHybridSample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ dwHybridSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dwHybridSample
+//
+//  Created by 성효빈 on 2015. 11. 4..
+//  Copyright © 2015년 dwHybridSample. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
